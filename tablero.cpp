6b2da6af00eb1e6473324bef3a989f1b6fd7253e
beaalderri <45493358+beaalderri@users.noreply.github.com>
2019-02-20 19:22:12 +0100

    actualizado

--- tablero.cpp
+++ tablero.cpp
@@ -1,9 +1,13 @@
 // Beatriz Álvarez de Arriba y Laurence Apuya Pangilinan
 
 #include "tablero.h"
+#include "casilla.h"
 #include <fstream>
 #include <iostream>
 using namespace std;
+
+bool comprobarFilaColumna(int fila, int col);
+bool comprobarNumero(int c);
 
 void iniciaTablero(tTablero tablero)
 {
@@ -14,7 +18,7 @@ void iniciaTablero(tTablero tablero)
 
 bool cargarTablero(string nombreFichero, tTablero tablero)
 {
-	bool abierto;
+	bool abierto, fija;
 	ifstream entrada;
 	string basura;
 	char c;
@@ -46,29 +50,71 @@ void dibujarTablero(const tTablero tablero)
 	for (int i = 0; i < MAX_FILAS; i++)
 	{
 		for (int j = 0; j < MAX_COLUMNAS; j++)
-			dibujaCasilla (tablero[i][j]);
+			dibujaCasilla(tablero[i][j]);
 		cout << endl;
 	}
 }
 
 bool ponerNum(tTablero tablero, int fila, int col, int c)
 {
+	bool posible = false;
 
+	if (comprobarFilaColumna(fila, col) && comprobarNumero(c) && comprobarCasillaVacia(tablero[fila][col])) {
+		tablero[fila][col].numero = c;
+		tablero[fila][col].estado_casilla = c_rellena;
+		posible = true;
+	}
+
+	return posible;
+}
+
+bool comprobarFilaColumna(int fila, int col) {
+	return fila >= 1 && fila <= 9 && col >= 1 && col <= 9;
+}
+
+bool comprobarNumero(int c) {
+	return c >= 1 && c <= 9;
 }
 
 bool borrarNum(tTablero tablero, int fila, int col)
 {
+	bool posible = false;
 
+	if (comprobarFilaColumna(fila, col) && comprobarCasillaRellena(tablero[fila][col])) {
+		tablero[fila][col].numero = 0;
+		tablero[fila][col].estado_casilla = c_vacia;
+		posible = true;
+	}
+
+	return posible;
 }
 
 bool tableroLleno(const tTablero tablero)
 {
+	bool relleno = false;
+	int i = 0;
 
+	while (i < MAX_FILAS && !relleno){
+		int j = 0;
+		while (j < MAX_COLUMNAS && !relleno) {
+			if (comprobarCasillaVacia(tablero[i][j]))
+				relleno = true;
+			else
+				j++;
+		}
+		if (!relleno)
+			i++;
+	}
+
+	return relleno;
 }
 
 void mostrarPosibles(const tTablero tablero, int fila, int col)
 {
-
+	if (comprobarFilaColumna(fila, col))
+		mostrar(tablero[fila][col].conjunto);
+	else
+		cout << "Las coordenadas no estan dentro de Sudoku" << endl;
 }
 
 void rellenarSimples(tTablero tablero)
