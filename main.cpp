53658c37f804479dd0267d4f29de86f510c1e9d8
Laurence <39163357+lapyren@users.noreply.github.com>
2019-03-23 11:06:12 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,49 @@
+// Beatriz Álvarez de Arriba y Laurence Apuya Pangilinan
+
+#include <iostream>
+#include <string>
+#include "listaSudokus.h"
+#include "juego.h"
+using namespace std;
+
+void mostrarMenu();
+int menu();
+
+int main(){
+	int opcion, puntos;
+	tListaSudokus lista;
+	tJuego juego;
+
+	creaListaVacia(lista);
+	if (cargar(lista)) {
+		opcion = menu();
+		while (opcion != 0) {
+			mostrar(lista);
+			puntos = jugarUnSudoku(juego.sudoku);
+			opcion = menu();
+		}
+	}
+	system("pause");
+	return 0;
+}
+
+void mostrarMenu(){
+	cout << "1. Jugar" << endl;
+	cout << "0. Salir" << endl;
+	cout << "Elige una opcion: ";
+}
+
+int menu(){
+	int opcion;
+
+	mostrarMenu();
+	cin >> opcion;
+	while (opcion < 0 || opcion > 1)
+	{
+		cout << "Opcion incorrecta" << endl;
+		mostrarMenu();
+		cin >> opcion;
+	}
+
+	return opcion;
+}
